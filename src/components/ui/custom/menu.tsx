d548053bf6b99e135d7234e00ebc5e6597714cf7
Catherine Yang <52cathyang@gmail.com>
2024-09-12 09:46:13 -0400

    fix sizing

--- src/components/ui/custom/menu.tsx
+++ src/components/ui/custom/menu.tsx
@@ -5,7 +5,7 @@ import { Button } from '@/components/ui/button'
 
 function Menu() {
   return (
-    <div className='flex items-center mx-56 my-56 flex-col'>
+    <div className='flex items-center mx-5 sm:mx-10 md:mx-32 lg:mx-56 my-56 flex-col'>
         <IconBeach stroke={2} size={60} />
         <h1 className="font-extrabold text-[32px]">
           101districts
